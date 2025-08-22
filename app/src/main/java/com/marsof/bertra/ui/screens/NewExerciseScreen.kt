92c9ca5b56c04a28076695149a51c09643e9c597
Sergei Markov <smarkov.dev@gmail.com>
2025-08-22 06:39:33 +0300

    A new exercise saving.

--- app/src/main/java/com/marsof/bertra/ui/screens/NewExerciseScreen.kt
+++ app/src/main/java/com/marsof/bertra/ui/screens/NewExerciseScreen.kt
@@ -1,19 +1,25 @@
 package com.marsof.bertra.ui.screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.marsof.bertra.R
+import com.marsof.bertra.data.entites.Exercise
 import com.marsof.bertra.ui.ViewModelProvider
 import com.marsof.bertra.ui.elements.ApplicationTopBar
 import com.marsof.bertra.ui.navigation.INavigationDestination
 import com.marsof.bertra.ui.viewmodels.NewExerciseScreenViewModel
+import kotlinx.coroutines.launch
 
 object NewExerciseScreenDestination : INavigationDestination {
     override val route: String get() = "new_exercise"
@@ -24,9 +30,20 @@ object NewExerciseScreenDestination : INavigationDestination {
 @Composable
 fun NewExerciseScreen(
     navigateToExerciseListScreen: () -> Unit,
-    viewModel: NewExerciseScreenViewModel = viewModel(factory = ViewModelProvider.AppViewModelProvider),
+    viewModel: NewExerciseScreenViewModel = viewModel(
+        factory = ViewModelProvider.AppViewModelProvider
+    ),
     openDrawer: () -> Unit
 ) {
+    val coroutineScope = rememberCoroutineScope()
+    val exerciseUiState = viewModel.exerciseFormUiState
+    val onSaveClick: () -> Unit = {
+        coroutineScope.launch {
+            viewModel.saveExercise()
+            navigateToExerciseListScreen()
+        }
+    }
+
     Scaffold(
         topBar = {
             ApplicationTopBar(
@@ -35,10 +52,40 @@ fun NewExerciseScreen(
             )
         },
     ) { innerPadding ->
-        Text(
-            text = "New Exercise",
-            style = MaterialTheme.typography.titleLarge,
-            modifier = Modifier.padding(innerPadding)
+        Column(
+            modifier = Modifier.padding(innerPadding),
+        ) {
+            ExerciseInputForm(
+                exerciseDetails = exerciseUiState.exercise,
+                onValueChange = viewModel::updateUiState,
+                modifier = Modifier.fillMaxWidth()
+            )
+            Button(
+                onClick = onSaveClick,
+                enabled = exerciseUiState.isEntryValid,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(stringResource(R.string.save_exercise))
+            }
+        }
+    }
+}
+
+@Composable
+fun ExerciseInputForm(
+    exerciseDetails: Exercise,
+    onValueChange: (Exercise) -> Unit = {},
+    modifier: Modifier
+) {
+    Column(
+        modifier = modifier,
+    ) {
+        OutlinedTextField(
+            value = exerciseDetails.name,
+            onValueChange = { onValueChange(exerciseDetails.copy(name = it)) },
+            label = { Text(stringResource(R.string.exercise_name_label)) },
+            modifier = Modifier.fillMaxWidth(),
+            singleLine = true
         )
     }
 }
--- app/src/main/java/com/marsof/bertra/ui/viewmodels/NewExerciseScreenViewModel.kt
+++ app/src/main/java/com/marsof/bertra/ui/viewmodels/NewExerciseScreenViewModel.kt
@@ -1,7 +1,38 @@
 package com.marsof.bertra.ui.viewmodels
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.marsof.bertra.data.dao.ExerciseDao
+import com.marsof.bertra.data.entites.Exercise
 
-class NewExerciseScreenViewModel(exerciseDao: ExerciseDao) : ViewModel() {
+data class ExerciseFormUiState(
+    val exercise: Exercise = Exercise(
+        id = 0,
+        name = ""
+    ),
+    val isEntryValid: Boolean = false
+)
+
+class NewExerciseScreenViewModel(private val exerciseDao: ExerciseDao) : ViewModel() {
+    var exerciseFormUiState by mutableStateOf(ExerciseFormUiState())
+
+    fun updateUiState(exercise: Exercise) {
+        exerciseFormUiState = ExerciseFormUiState(
+            exercise = exercise,
+            isEntryValid = validateInput(exercise)
+        )
+    }
+
+    private fun validateInput(exercise: Exercise = exerciseFormUiState.exercise): Boolean {
+        return exercise.name.isNotBlank()
+    }
+
+    suspend fun saveExercise() {
+        if (validateInput()) {
+            exerciseDao.insert(exerciseFormUiState.exercise)
+        }
+    }
+
 }

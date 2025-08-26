c93631ba72c66284ee00d3efd0af4d5aa939d002
Sergei Markov <smarkov.dev@gmail.com>
2025-08-26 08:23:51 +0300

    cleanup

--- app/src/main/java/com/marsof/bertra/ui/screens/AddTrainExerciseScreen.kt
+++ app/src/main/java/com/marsof/bertra/ui/screens/AddTrainExerciseScreen.kt
@@ -6,13 +6,11 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.toString
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel

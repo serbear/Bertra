ec64142e18570302382e455553707368940cd47e
Sergei Markov <smarkov.dev@gmail.com>
2025-09-09 10:34:29 +0300

    Implementing the workout engaging process...

--- app/src/main/java/com/marsof/bertra/ui/screens/WorkoutEngageScreen.kt
+++ app/src/main/java/com/marsof/bertra/ui/screens/WorkoutEngageScreen.kt
@@ -4,7 +4,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Rocket
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -15,6 +21,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.marsof.bertra.R
 import com.marsof.bertra.ui.ViewModelProvider
@@ -90,7 +97,6 @@ fun WorkoutEngageScreen(
                         items(
                             items = workoutTrainListState,
                             key = { it.trainExercise.id }) { currentExercise ->
-//                            SingleTrainExercise(trainExercise = currentExercise)
                             Text(
                                 text = currentExercise.trainExercise.exerciseId.toString()
                             )
@@ -101,9 +107,22 @@ fun WorkoutEngageScreen(
                     }
                 }
                 //
-                // todo: Engage Button
+                // Engage Button
                 //
-                // enable = workoutTrainListState.isEmpty()
+                Button(
+                    onClick = { navigateToActiveWorkoutScreen(workoutId) },
+                    modifier = Modifier,
+                    shape = RoundedCornerShape(0.dp),
+                    enabled = workoutTrainListState.isNotEmpty()
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Rocket,
+                        contentDescription = stringResource(R.string.workout_engage_button_label),
+                    )
+                    Text(
+                        text = stringResource(R.string.workout_engage_button_label),
+                    )
+                }
             }
         }
     }
